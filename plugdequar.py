51284479a8d19ac7e7c8b3c7821b00e6f55cfe9a
Chris Kenrick <pelennor@gmail.com>
2022-07-03 06:08:59 +1000

    bare bones working

--- plugdequar.py
+++ plugdequar.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 from pathlib import Path
 import subprocess
+import xattr
 
-baseDir = Path.cwd()
-for fn in baseDir.rglob('*.wav') :
-    newfn = fn.with_suffix('.flac')
-    extCmd = ['ffmpeg', '-i', str(fn), str(newfn)]
-    pout2 = subprocess.run(extCmd, capture_output=True)
-    if pout2.returncode == 0 :
-        fn.unlink()
-    else :
-        print(str(fn))
+QUARANTINE_KEY = 'com.apple.quarantine'
+
+
+
+baseDir = Path('/Library/Audio/Plug-Ins')
+
+#for f in [i for i in baseDir.iterdir() if i.is_file()] :
+#    print(f)
+
+for d in [i for i in baseDir.rglob('*.vst') if i.is_dir()]:
+    xa = xattr.xattr(d)
+    if QUARANTINE_KEY in xa.keys():
+        del xa[QUARANTINE_KEY]
+        print(d)
+
+
+
+# print(baseDir)
+# print(dir(baseDir))
+
+# baseDir = Path.cwd()
+# for fn in baseDir.rglob('*.wav') :
+#     newfn = fn.with_suffix('.flac')
+#     extCmd = ['ffmpeg', '-i', str(fn), str(newfn)]
+#     pout2 = subprocess.run(extCmd, capture_output=True)
+#     if pout2.returncode == 0 :
+#         fn.unlink()
+#     else :
+#         print(str(fn))
     
 
     #print(s)
